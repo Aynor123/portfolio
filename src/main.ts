09b38efdfff96db37064ffec8e8eda4779c7f23b
Julian W <Julian.wuenkhaus@gmx.net>
2024-11-16 22:32:54 +0100

    added herso section

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,14 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+/* This code snippet is adding an event listener to the document for the 'mousemove' event. When the
+user moves the mouse, the callback function provided will be executed. */
+document.addEventListener('mousemove', (event) => {
+  const shadow = document.querySelector('.cursor-shadow') as HTMLElement;
+  if (shadow) {
+    shadow.style.left = `${event.pageX}px`;
+    shadow.style.top = `${event.pageY}px`;
+  }
+});
